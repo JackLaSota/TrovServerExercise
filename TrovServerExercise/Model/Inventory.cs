ae97052230c505c6038b3f260b73e4d05305741d
Jack_LaSota <jack.lasota@gmail.com>
2016-02-06 23:11:17 -0800

    Added skeletal model classes.

---
+++ TrovServerExercise/Model/Inventory.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+
+namespace TrovServerExercise.Model {
+	public class Inventory {
+		public List<Item> items = new List<Item>();
+	}
+}
---
+++ TrovServerExercise/Model/Item.cs
@@ -0,0 +1,7 @@
+namespace TrovServerExercise.Model {
+	public class Item {
+		public string Name {get; set;}
+		public string Description {get; set;}
+		public int Price {get; set;}
+	}
+}
