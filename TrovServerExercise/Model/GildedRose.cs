c889f6c7de314ed116055b9e7bc9f0e7495a84e6
Jack_LaSota <jack.lasota@gmail.com>
2016-02-07 10:43:17 -0800

    Added MoneyInRegister.

--- TrovServerExercise/Model/GildedRose.cs
+++ TrovServerExercise/Model/GildedRose.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 namespace TrovServerExercise.Model {
 	public partial class GildedRose {
 		Inventory inventory = new Inventory();
+		public uint MoneyInRegister {get; set;}
 		public IEnumerable<Item> ItemsForSale {get {return inventory.items;}}
 	}
 }
