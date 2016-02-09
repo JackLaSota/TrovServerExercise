51aad3817e85d53d250943ad1cbbb7262574170f
Jack_LaSota <jack.lasota@gmail.com>
2016-02-08 22:08:54 -0800

    Deleted dead code.

--- TrovServerExercise/Model/Customer.cs
+++ TrovServerExercise/Model/Customer.cs
@@ -2,7 +2,6 @@ using JetBrains.Annotations;
 
 namespace TrovServerExercise.Model {
 	public partial class Customer {
-		//[CanBeNull] Session currentSession;
 		[NotNull] public readonly string name;//Not guaranteed unique.
 		[NotNull] public readonly string username;//Guaranteed unique. //Invariant: in Unicode normalized form C.
 		[NotNull] string password;//Invariant: in Unicode normalized form C. //todo Salt instead of storing this. (Make sure to normalize before salting).
@@ -16,8 +15,5 @@ namespace TrovServerExercise.Model {
 		public bool PasswordIs (string allegedPassword) {return allegedPassword.Normalize() == password;}
 		public uint MoneyInWallet {get; set;}
 		public bool CanAfford (Item item) {return item.Price <= MoneyInWallet;}
-		/*public void BeginSession () {
-			//todo dispose of old session?
-		}*/
 	}
 }
