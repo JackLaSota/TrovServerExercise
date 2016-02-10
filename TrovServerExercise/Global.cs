1295f002af945de15f1b976cb30272069c7558fb
Jack_LaSota <jack.lasota@gmail.com>
2016-02-10 00:03:38 -0800

    Deleted dead code.

--- TrovServerExercise/Global.cs
+++ TrovServerExercise/Global.cs
@@ -4,19 +4,9 @@ using System.ServiceModel;
 namespace TrovServerExercise {
 	public static partial class Global {
 		#region Copied_From_My_Game
-		// ReSharper disable once MemberCanBePrivate.Global
-		public static S GetWrappingExceptions <T, S> (Func<S> toTry, string exceptionMessage, Func<Exception, bool> acceptor) where T : GildedRoseException {
-			var toReturn = default(S);//Never used. If assignment fails then this throws instead of returning.
-			DoWrappingExceptions<T>(() => toReturn = toTry(), exceptionMessage, acceptor);
-			return toReturn;
-		}
-		public static S GetWrappingExceptions <T, S> (Func<S> toTry, string exceptionMessage) where T : GildedRoseException {
-			return GetWrappingExceptions<T, S>(toTry, exceptionMessage, e => true);
-		}
 		public static void DoWrappingExceptions <T> (Action toTry, string exceptionMessage) where T : GildedRoseException {
 			DoWrappingExceptions<T>(toTry, exceptionMessage, e => true);
 		}
-		// ReSharper disable once MemberCanBePrivate.Global
 		public static void DoWrappingExceptions <T> (Action toTry, string exceptionMessage, Func<Exception, bool> acceptor) where T : GildedRoseException {
 			try {toTry();}
 			catch (Exception caught) {
